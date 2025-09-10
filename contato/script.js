f64063657976270702015993001cb790ecf09598
matheus1902-code <matheusvm2019@gmail.com>
2025-09-10 08:14:27 -0300

    Add files via upload

--- contato/script.js
+++ contato/script.js
@@ -1,3 +1,14 @@
-function q(){
-    
-}
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('contatoForm');
+  const feedback = document.getElementById('form-feedback');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault(); // impede o envio real do formulário
+
+    // Mostra a mensagem de sucesso
+    feedback.classList.remove('hidden');
+
+    // Opcional: limpa os campos do formulário
+    form.reset();
+  });
+});
